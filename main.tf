ee29a3235ee3a3c91b814520006761ccedb2dfda
Lee heechan <a_line1@naver.com>
2021-03-02 20:53:52 +0900

    Add security groups

--- main.tf
+++ main.tf
@@ -2,10 +2,12 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+# Create VPC
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.75.0"
 
+  # Note! Internet Gateway automatically created with name of VPC. Then it will be attached to this VPC.
   name = "nextcloud-terraform"
 
   azs             = ["ap-northeast-2a", "ap-northeast-2b"]
@@ -13,10 +15,94 @@ module "vpc" {
   public_subnets  = ["10.10.0.0/18", "10.10.64.0/18"]
   private_subnets = ["10.10.128.0/18", "10.10.192.0/18"]
 
+  # Enable DNS hostname and DNS resolution. These are required for EFS mount.
   enable_dns_hostnames = true
   enable_dns_support   = true
 }
 
+resource "aws_security_group" "allow-ssh" {
+  name = "Allow_SSH"
+  description = "Allow SSH inbound traffic"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    description = "Allow SSH traffic from everywhere"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow-http" {
+  name = "Allow_HTTP"
+  description = "Allow HTTP inbound traffic"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    description = "Allow HTTP traffic from everywhere"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "allow-https" {
+  name = "Allow_HTTPS"
+  description = "Allow HTTPS inbound traffic"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    description = "Allow HTTPS traffic from everywhere"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "allow-nfs" {
+  name = "Allow_NFS"
+  description = "Allow NFS inbound traffic"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    description = "Allow NFS traffic from everywhere"
+    from_port = 2049
+    to_port = 2049
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 2049
+    to_port = 2049
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Ubuntu Bionic. Ubuntu 18.04 LTS AMI
 data "aws_ami" "ubuntu-bionic" {
   most_recent = true
 
@@ -36,6 +122,8 @@ data "aws_ami" "ubuntu-bionic" {
 resource "aws_instance" "simple1" {
   ami           = data.aws_ami.ubuntu-bionic.id
   instance_type = "t3.micro"
+  key_name      = "key4test"
 
-  subnet_id = module.vpc.public_subnets[0]
+  subnet_id              = module.vpc.public_subnets[0]
+  security_groups = [aws_security_group.allow-ssh.id, aws_security_group.allow-http.id, aws_security_group.allow-https.id, aws_security_group.allow-nfs.id]
 }
