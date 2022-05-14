e0407d927ffe65c9bc970d11d295a1c53cc57860
Lee heechan <a_line1@naver.com>
2022-05-14 16:10:08 +0900

    main.tf 변경사항: Route53를 추가

--- main.tf
+++ main.tf
@@ -451,3 +451,22 @@ resource "aws_datasync_task" "migrate" {
     IaCTool = "Terraform"
   }
 }
+
+# 2022-05-12 #
+# Route53
+
+data "aws_route53_zone" "myworld" {
+  zone_id = "Z1044780QS7P5JPQT0A8"
+  tags = {
+    IaCTool = "Terraform"
+  }
+}
+
+# Add "A Record"
+resource "aws_route53_record" "nextcloud" {
+  zone_id = data.aws_route53_zone.myworld.zone_id
+  name    = "nextcloud.${data.aws_route53_zone.myworld.name}"
+  type    = "A"
+  ttl     = "300"
+  records = ["3.35.95.62"]
+}
