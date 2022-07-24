ce99e0e4a774d576bc7fb79d88bb080398cebf29
Lee heechan <a_line1@naver.com>
2022-07-24 20:02:56 +0900

    main.tf 변경사항: Kinesis Firehose의 buffer_interval 조정

--- main.tf
+++ main.tf
@@ -485,6 +485,7 @@ resource "aws_kinesis_firehose_delivery_stream" "nextcloud-stream" {
   s3_configuration {
     role_arn   = aws_iam_role.kinesis-role.arn
     bucket_arn = module.store-metric.s3_bucket_arn
+    buffer_interval = 900
   }
 
   tags = {
