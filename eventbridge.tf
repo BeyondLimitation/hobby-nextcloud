cbc7fbcf12b817079c1ab58150cd76a3e255bb56
Lee heechan <a_line1@naver.com>
2023-07-01 22:00:05 +0900

    Add target

--- eventbridge.tf
+++ eventbridge.tf
@@ -4,4 +4,14 @@ resource "aws_cloudwatch_event_rule" "nextcloud-notrunning" {
 
   # Load Event Pattern JSON file.
   event_pattern = templatefile("eventbridge/nextcloud-notrunning.tmp.json", { nextcloud-instance = aws_instance.nextcloud-instance.id })
+
+  tags ={
+    "IaCTool" = "Terraform"
+  }
+}
+
+# Add Event Rule target
+resource "aws_cloudwatch_event_target" "email-alert" {
+  rule = aws_cloudwatch_event_rule.nextcloud-notrunning.name
+  arn  = aws_sns_topic.nextcloud-notrunning.arn
 }
