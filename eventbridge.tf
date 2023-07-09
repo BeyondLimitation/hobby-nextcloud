9d2ed0c74158d6dc83c7684d9d09b3c871de38e3
Lee heechan <a_line1@naver.com>
2023-07-09 18:00:51 +0900

    fixed somewhere broken. add policy

--- eventbridge.tf
+++ eventbridge.tf
@@ -5,7 +5,7 @@ resource "aws_cloudwatch_event_rule" "nextcloud-notrunning" {
   # Load Event Pattern JSON file.
   event_pattern = templatefile("eventbridge/nextcloud-notrunning.tmp.json", { nextcloud-instance = aws_instance.nextcloud-instance.id })
 
-  tags ={
+  tags = {
     "IaCTool" = "Terraform"
   }
 }
@@ -14,4 +14,12 @@ resource "aws_cloudwatch_event_rule" "nextcloud-notrunning" {
 resource "aws_cloudwatch_event_target" "email-alert" {
   rule = aws_cloudwatch_event_rule.nextcloud-notrunning.name
   arn  = aws_sns_topic.nextcloud-notrunning.arn
+
+  input_transformer {
+    input_paths = {
+      instance-id = "$.detail.instance-id",
+      state       = "$.detail.state"
+    }
+    input_template = "\"Instance <instance-id> is in <state>\""
+  }
 }
--- sns.tf
+++ sns.tf
@@ -1,6 +1,15 @@
 # Create SNS Topic
 resource "aws_sns_topic" "nextcloud-notrunning" {
-  name = "NextCloud-NotRunning"
+  name   = "NextCloud-NotRunning"
+
+  tags = {
+    "IaCTool" = "Terraform"
+  }
+}
+
+resource "aws_sns_topic_policy" "proper-one" {
+  arn = aws_sns_topic.nextcloud-notrunning.arn
+  policy = templatefile("./iam/sns/allow_events.tpl.json", { nextcloud-notrunning = aws_sns_topic.nextcloud-notrunning.arn })
 }
 
 # Create SNS Topic Subscription. Send to the E-mail specified
