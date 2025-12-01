48a8c002f0d83543444c86a64c82bac9fc6dff9d
lee heechan <leeheechan1@gmail.com>
2025-12-01 18:13:18 +0900

    AWS DataSync Location 설정

--- migrate-data.tf
+++ migrate-data.tf
@@ -7,3 +7,12 @@ resource "aws_s3_bucket" "efs2s3" {
     "IaCTool" = "Terraform"
   }
 }
+
+resource "aws_datasync_location_efs" "datasync-mount-target" {
+  efs_file_system_arn = aws_efs_file_system.efs4nextcloud.arn
+
+  ec2_config {
+    security_group_arns = [module.nextcloud-ng.security_group_arn]
+    subnet_arn          = module.vpc.private_subnet_arns[0]
+  }
+}
